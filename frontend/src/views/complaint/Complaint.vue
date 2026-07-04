c9a2b27a364df74e1bd13af95574e288af685a1e
向乾坤 <2860809459@qq.com>
2026-07-04 16:59:11 +0800

    部分页面初始化

---
+++ frontend/src/views/complaint/Complaint.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card>
+    <el-table :data="list">
+      <el-table-column prop="content" label="投诉内容"/>
+      <el-table-column prop="status" label="状态"/>
+    </el-table>
+  </el-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const list = ref([
+  { content: '噪音问题', status: '已处理' }
+])
+</script>

9f736b9f5ae46a9c73bb5c52dc7e96c2cb8a658d
yulin <15947750+yulin--jun@user.noreply.gitee.com>
2026-07-04 09:25:45 +0800

    拦截器，数据库默认值填充

---
+++ src/main/java/com/property_management/config/MyMetaObjectHandler.java
@@ -0,0 +1,63 @@
+package com.property_management.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.property_management.utils.ThreadLocalUtil;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+
+        Long currentUserId = ThreadLocalUtil.getCurrentUserId();
+
+        if (currentUserId == null) {
+            currentUserId = 0L;
+        }
+
+        this.strictInsertFill(metaObject,
+                "version",
+                Integer.class,
+                1);
+
+        this.strictInsertFill(metaObject,
+                "createTime",
+                LocalDateTime.class,
+                LocalDateTime.now());
+
+        this.strictInsertFill(metaObject,
+                "updateTime",
+                LocalDateTime.class,
+                LocalDateTime.now());
+
+        this.strictInsertFill(metaObject,
+                "createUser",
+                Long.class,
+                currentUserId);
+
+        this.strictInsertFill(metaObject,
+                "updateUser",
+                Long.class,
+                currentUserId);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+        Long currentUserId = ThreadLocalUtil.getCurrentUserId();
+
+        this.strictUpdateFill(metaObject,
+                "updateTime",
+                LocalDateTime.class,
+                LocalDateTime.now());
+
+        this.strictUpdateFill(metaObject,
+                "updateUser",
+                Long.class,
+                currentUserId);
+    }
+}
---
+++ src/main/java/com/property_management/interceptors/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.property_management.interceptors;
+
+import com.property_management.utils.JwtUtil;
+import com.property_management.utils.ThreadLocalUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 处理预检请求
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true; // 对OPTIONS预检请求直接放行
+        }
+
+        //token获取
+        String token = request.getHeader("Authorization");
+        try {
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            //把业务数据存储到ThreadLocal中
+            ThreadLocalUtil.set(claims);
+            //放行
+            return true;
+        } catch (Exception e) {
+            //不放行
+            response.setStatus(401);
+            return false;
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        ThreadLocalUtil.remove();
+    }
+}
